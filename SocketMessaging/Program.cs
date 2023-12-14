1ec4431e3275f10736bbbddbeda4b1f959950504
chadjewell <chadwickjewell@gmail.com>
2023-12-14 16:18:41 -0600

    Add project files.

---
+++ SocketMessaging/Program.cs
@@ -0,0 +1,56 @@
+﻿using SocketMessaging;
+
+// See https://aka.ms/new-console-template for more information
+
+// Prompt for IP address
+//Console.WriteLine("Enter IP Address:");
+//string ipAddress = Console.ReadLine();
+string ipAddress = "10.0.0.147";
+
+// Prompt for Port
+//Console.WriteLine("Enter Port #");
+//int port = int.Parse(Console.ReadLine());
+int port = 4545;
+
+// Acknowledge Connect
+Console.WriteLine("Press Enter to Connect");
+Console.ReadKey();
+
+SocketClient inSight = new SocketClient(ipAddress, port);
+//Console.WriteLine(inSight.Connect().ToString());
+
+List<string> response = inSight.Connect();
+foreach (string line in response)
+{
+    Console.WriteLine(line);
+}
+
+// Prompt for Username
+string msg = Console.ReadLine();
+response = inSight.Send(msg);
+foreach (string line in response)
+{
+    Console.WriteLine(line);
+}
+
+// Prompt for Password
+msg = Console.ReadLine();
+response = inSight.Send(msg);
+foreach (string line in response)
+{
+    Console.WriteLine(line);
+}
+
+// Prompt for Command
+msg = Console.ReadLine();
+response = inSight.Send(msg);
+foreach (string line in response)
+{
+    Console.WriteLine(line);
+}
+
+// Prompt for disconnect
+Console.WriteLine("Press Enter to Disconnect");
+Console.ReadKey();
+
+inSight.Disconnect();
---
+++ SocketMessaging/SocketClient.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketMessaging
+{
+    internal class SocketClient
+    {
+        private IPAddress _address;
+        private IPEndPoint _remoteEP;
+        private Socket _socket;
+        // Constructor for the socket client, requires string IP address and int port
+        public SocketClient(string ipAddress, int port)
+        {
+            _address = IPAddress.Parse(ipAddress);
+            _remoteEP = new IPEndPoint(_address, port);
+
+            _socket = new Socket(_address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            _socket.ReceiveTimeout = 1000;
+        }
+
+        // Use the connect method to establish a connection with the remote device
+        public List<string> Connect()
+        {
+            _socket.Connect(_remoteEP);
+
+            //var temp = this.Receive();
+
+            return this.Receive();
+        }
+        
+        public List<string> Send(string msg)
+        {
+            //_socket.Blocking = false;
+
+            // Send message to the remote device
+            byte[] bMsg = Encoding.ASCII.GetBytes(msg + "\r\n");
+            int bytesSent = _socket.Send(bMsg);
+
+            return this.Receive();
+        }
+
+        // Release the socket
+        public void Disconnect()
+        {
+            _socket.Shutdown(SocketShutdown.Both);
+            _socket.Close();
+        }
+
+        private List<string> Receive()
+        {
+            // Receive the response from the remote device
+            byte[] buffer = new byte[1024];
+            int bytesRec = _socket.Receive(buffer);
+            Thread.Sleep(100);
+
+            // Create a list to store the received bytes
+            List<byte> byteList = new List<byte>(buffer.Take(bytesRec));            
+
+            // While there is more data available, receive it and add it to the list
+            while (_socket.Available > 0)
+            {
+                bytesRec = _socket.Receive(buffer);
+                byteList.AddRange(buffer.Take(bytesRec));
+            }
+
+            // Convert the list of bytes to a string
+            string response = Encoding.ASCII.GetString(byteList.ToArray());
+
+            // Split the string into an array of messages using the <EOM> delimeter
+            string[] messageArray = response.Split(new string[] { "<EOM>" }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Create a list to store the messages
+            List<string> messages = new List<string>();
+
+            // Add each message to the list
+            messages.AddRange(messageArray);
+
+            // Return the list of received strings
+            return messages;
+        }
+
+    }
+}
