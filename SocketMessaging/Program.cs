f0427595e684d9ec8513b85c217c7fa35932f81a
chadjewell <chadwickjewell@gmail.com>
2023-12-14 16:22:44 -0600

    added IP and port input

--- SocketMessaging/Program.cs
+++ SocketMessaging/Program.cs
@@ -3,14 +3,14 @@
 // See https://aka.ms/new-console-template for more information
 
 // Prompt for IP address
-//Console.WriteLine("Enter IP Address:");
-//string ipAddress = Console.ReadLine();
-string ipAddress = "10.0.0.147";
+Console.WriteLine("Enter IP Address:");
+string ipAddress = Console.ReadLine();
+//string ipAddress = "10.0.0.147";
 
 // Prompt for Port
-//Console.WriteLine("Enter Port #");
-//int port = int.Parse(Console.ReadLine());
-int port = 4545;
+Console.WriteLine("Enter Port #");
+int port = int.Parse(Console.ReadLine());
+//int port = 4545;
 
 // Acknowledge Connect
 Console.WriteLine("Press Enter to Connect");
